f70709bb005a88a4d7716d40f1575934495137ec
lothar-mar <lothar.mar@gmail.com>
2013-01-08 23:10:47 +0000

    added code to  git.update.sh

--- git.update.sh
+++ git.update.sh
@@ -1,1 +1,15 @@
 #!/bin/bash
+# This shell script will update all git remote repositories in the machine 
+automatically (using the fetch command).
+
+###############
+
+# Repository Setup 
+
+
+
+
+##############
+
+# Updating repos
+
