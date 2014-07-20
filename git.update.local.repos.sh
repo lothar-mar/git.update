1d5fede2e7f67226f0854928eff0f57cf9478a93
lothar-mar <lothar.mar@gmail.com>
2014-07-20 16:18:27 +0100

    Added support to vpn.scripts

--- git.update.local.repos.sh
+++ git.update.local.repos.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # This shell script will update all git remote repositories in the machine automatically (using the fetch command).
 
-###############
 
+##################################################
 # Updating repos (lothar_m's repos)
 cd ~/local.repository/bash.scripts/git.update
 git fetch origin --verbose
@@ -10,16 +10,19 @@ git fetch origin --verbose
 cd ~/local.repository/bash.scripts/git.update.remote.repos
 git fetch origin --verbose
 
-cd ~/local.repository/bash.scripts/system.update
-git fetch origin --verbose
-
 cd ~/local.repository/bash.scripts/SpiderOak.maintenance
 git fetch origin --verbose
 
-cd ~/local.repository/bash.scripts/get.quotes.yahoo
+cd ~/local.repository/bash.scripts/vpn.scripts
 git fetch origin --verbose
 
-# Updating repos (others)
+cd ~/local.repository/dotfiles
+git fetch origin --verbose
+
+
+######################################
+# Updating third party repos
+
 cd ~/local.repository/electrum
 git fetch origin --verbose
 
@@ -28,3 +31,35 @@ git fetch origin --verbose
 
 cd ~/local.repository/dotfiles
 git fetch origin --verbose
+
+cd ~/local.repository/bash.scripts/get.quotes.yahoo
+git fetch origin --verbose
+
+cd ~/local.repository/gsclient
+git fetch origin --verbose
+
+cd ~/local.repository/tulpenmanie
+git fetch origin --verbose
+
+cd ~/local.repository/icsv2ledger
+git fetch origin --verbose
+
+cd ~/local.repository/commafeed
+git fetch origin --verbose
+git push openshift master
+
+cd ~/local.repository/goxtool
+git fetch origin --verbose
+
+cd ~/local.repository/goxgui
+git fetch origin --verbose 
+
+cd ~/local.repository/Open-Transactions
+git fetch origin --verbose 
+
+cd ~/local.repository/Moneychanger
+git fetch origin --verbose 
+    
+
+
+
